7edf69287b430f495b6e0f17555ba4abc30e644e
rizman768 <rizqimaulana768@gmail.com>
2022-07-03 13:56:00 +0700

    perbaikan

--- src/routes/route-inventori.js
+++ src/routes/route-inventori.js
@@ -1,32 +1,9 @@
 require('dotenv').config();
-const jwt = require('jsonwebtoken');
 const router = require('express').Router();
 const { inventori } = require('../controllers');
 
-function AuthenticateAccessToken(req,res,next){
-    const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[2];
-    console.log(process.env.ACCESS_TOKEN_SECRET);
-    console.log(token);
-    
-    if(token == null){
-        res.json({ message: 'Invalid access token'});
-    }
-    else{
-        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err,user) => {
-            if(err){
-                res.json({ message: err });
-            }
-            else{
-                
-                next();
-            }
-        });
-    }
-}
-
 // GET localhost:8080/inventori => Ambil data semua inventori
-router.get('/inventori', AuthenticateAccessToken, inventori.getDataInventori);
+router.get('/inventori', inventori.getDataInventori);
 
 // POST localhost:8080/inventori/add => Tambah data inventori ke database
 router.post('/inventori/add', inventori.addDataInventori);
